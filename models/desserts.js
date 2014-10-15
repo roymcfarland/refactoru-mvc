661da48feafd55a0e20af0d58b2908d5f53ae678
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-15 10:49:46 -0600

    first & final commit

---
+++ models/desserts.js
@@ -0,0 +1,22 @@
+var desserts = [
+	{
+	name: 'Tiramisu',
+	flavor: 'Cinnamon',
+	rating: 'Super good!',
+	cost: 'Too much'
+	},
+	{
+	name: 'Figgy Pudding',
+	flavor: 'Figgy',
+	rating: 'Disgusting',
+	cost: '1 miiiiiillion dollars'
+	},
+	{
+	name: 'Chocolate Covered Shrimp',
+	flavor: 'Chocolatey Shrimpish',
+	rating: 0,
+	cost: 'half dollar'
+	}
+];
+
+module.exports = desserts;
